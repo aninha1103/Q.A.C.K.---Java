7a1833b31d02ed542d40c53834197be21356f326
JoaoPolar <jn459998@gmail.com>
2024-05-31 10:10:48 -0300

    Ajuste do caminho das bibliotecas

--- Q.A.C.K/src/Testes/ScriptBanco.java
+++ Q.A.C.K/src/Testes/ScriptBanco.java
@@ -2,7 +2,7 @@
     import java.sql.DriverManager;
     import java.sql.ResultSet;
     import java.sql.SQLException;
-    import java.sql.Statement;
+    import java.sql.Statement; 
 
     public class ScriptBanco
     {
